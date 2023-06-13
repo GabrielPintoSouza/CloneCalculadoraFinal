1cf50bbeec9f98ac9b642e7002d713367d8635af
Fabricio <fabriciojosemacedo@gmail.com>
2023-06-13 17:19:03 -0300

    Lendo 2 numero

--- calculadora.c
+++ calculadora.c
@@ -10,11 +10,15 @@ int subtracao(int num1, int num2){
     return resultado;
 }
 int main(){
-	int n1
-	int n2
+	int n1;
+	int n2;
 	
 	printf("Digite um numero: ")
 	scanf("%d", &n1);
+	
+	printf("Digite outro numero: ")
+	scanf("%d", &n2);
+	
     return 0;
 }
 
