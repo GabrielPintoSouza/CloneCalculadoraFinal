d79ab3bec03325ab324ec8f66a089cb08691fcf4
GabrielPS <gabriel.pinto@cefet-rj.br>
2023-06-13 17:18:27 -0300

    Implementação da função multiplicação

--- calculadora.c
+++ calculadora.c
@@ -9,11 +9,17 @@ int subtracao(int num1, int num2){
     resultado = num1 + num2;
     return resultado;
 }
+
+int multiplicacao(int num1, int num2){
+    int resultado;
+    resultado = num1*num2;
+    return resultado;
+}
 int main(){
-	int n1
-	int n2
-	
-	printf("Digite um numero: ")
+	int n1;
+	int n2;
+
+	printf("Digite um numero: ");
 	scanf("%d", &n1);
     return 0;
 }
