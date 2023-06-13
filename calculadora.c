17d4dad8399bd9eaf1c409ca6346d0e28a11ae5f
GabrielPS <gabriel.pinto@cefet-rj.br>
2023-06-13 17:35:57 -0300

    Implementacao do seletor de operacoes

--- calculadora.c
+++ calculadora.c
@@ -18,13 +18,25 @@ int multiplicacao(int num1, int num2){
 int main(){
 	int n1;
 	int n2;
+	char operacao;
 
-	printf("Digite um numero: ");
+	printf("Digite o caractere correspondente a operacao a ser executada: + (soma) - (subtracao) * (multiplicacao) / (divisao)\n");
+	scanf("%c", &operacao);
+
+	printf("Digite um numero: \n");
 	scanf("%d", &n1);
-	
-	printf("Digite outro numero: ");
+
+	printf("Digite outro numero: \n");
 	scanf("%d", &n2);
-	
+
+	switch(operacao){
+        case '+': printf("O resultado e: %d\n", soma(n1, n2));
+        case '-': printf("O resultado e: %d\n", subtracao(n1, n2));
+        case '*': printf("O resultado e: %d\n", multiplicacao(n1, n2));
+        case '/': printf("O resultado e: %d\n", divisao(n1, n2));
+        default: printf("Operacao invalida\n");
+	}
+
     return 0;
 }
 
