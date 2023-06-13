115aa2ca9e672339b45d77e567efaae349bcc8ed
GabrielPS <gabriel.pinto@cefet-rj.br>
2023-06-13 17:03:06 -0300

    Implementação da função subtração

--- calculadora.c
+++ calculadora.c
@@ -3,6 +3,12 @@ int soma(int num1, int num2){
     resultado = num1 + num2;
     return resultado;
 }
+
+int subtracao(int num1, int num2){
+    int resultado;
+    resultado = num1 + num2;
+    return resultado;
+}
 int main(){
     return 0;
 }
